49a4f35ce784d79b95859e9ccbc71afa4d32c33d
florentJava <njetejie.florent21@myiuc.com>
2024-09-05 04:23:09 +0100

    end of section 3

---
+++ test.js
@@ -0,0 +1,8 @@
+document.querySelector('.menu-toggle').addEventListener('click', function() {
+    const menu = document.querySelector('nav ul');
+    if (menu.style.display === 'flex') {
+        menu.style.display = 'none';
+    } else {
+        menu.style.display = 'flex';
+    }
+});
